91220539dd0e731e6c6524f92b1dc19f047f992b
Hyerim.Hwang <rimhoho@gmail.com>
2020-10-09 17:43:56 -0400

    git init

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
-import json
-import requests
-import flask
-from datetime import datetime
-from pandas.io.json import json_normalize
+from flask import Flask
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return render_template("index.html")
+def hello_world():
+    """Print 'Hello, world!' as the response body."""
+    return 'Hello, world!'
 
 if __name__ == "__main__":
     app.run(debug=True)
