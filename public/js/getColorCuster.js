4201b4ab475afeac001f431ee41966727e9605e0
rimhoho <rimhoho@gmail.com>
2020-11-08 01:52:18 -0500

    Extracted color scale data

---
+++ public/js/getColorCuster.js
@@ -0,0 +1,156 @@
+let colorWayNname = [{target: '350 V2 Natural', fullRGB: ["241 244 238", "219 211 194", "247 236 192"]}, 
+                    {target: 'QNTM Teal Blue', fullRGB: ["66 76 76", "53 140 154", "98 97 104"]}, 
+                    {target: '350 V2 Carbon', fullRGB: ["32 34 38", "87 88 95", "223 223 209"]}, 
+                    {target: '380 Pepper', fullRGB: ["208 196 184", "196 157 127", "100 101 109"]}, 
+                    {target: '700 V3 Arzareth', fullRGB: ["54 121 172", "145 176 186", "238 240 242"]}, 
+                    {target: '350 V2 Israfil', fullRGB: ["140 149 160", "180 146 105", "101 108 106"]}, 
+                    {target: '380 Blue Oat', fullRGB: ["170 130 100", "43 87 163", "122 95 79"]}, 
+                    {target: '350 V2 Zyon', fullRGB: ["172 167 160", "25 23 21", "247 244 231"]}, 
+                    {target: '700 MNVN Bone', fullRGB: ["239 234 224", "152 155 163", "53 55 59"]}, 
+                    {target: 'QNTM Barium', fullRGB: ["98 99 102", "21 23 22", "240 239 233"]}, 
+                    {target: '350 V2 Sulfur', fullRGB: ["182 175 134", "128 130 86", "203 201 160"]}, 
+                    {target: '700 MNVN Phosphor', fullRGB: ["220 230 120", "62 61 64", "225 227 233"]}, 
+                    {target: '350 V2 Linen', fullRGB: ["226 220 198", "132 140 149", "232 201 154"]}, 
+                    {target: '700 V3 Alvah', fullRGB: ["51 52 54", "63 67 72", "29 31 32"]}, 
+                    {target: '380 Mist', fullRGB: ["135 130 125", "209 169 137", "84 83 86"]}, 
+                    {target: '350 V2 Cinder', fullRGB: ["43 43 45", "30 32 33", "54 56 62"]}, 
+                    {target: '350 V2 Desert Sage', fullRGB: ["168 173 169", "249 139 112", "171 175 149"]}, 
+                    {target: '700 MNVN Orange', fullRGB: ["252 160 98", "44 44 46", "135 143 149"]}, 
+                    {target: '350 V2 Tail Light', fullRGB: ["204 202 204", "247 63 46", "149 150 154"]}, 
+                    {target: '350 V2 Earth', fullRGB: ["154 132 110", "62 56 51", "98 83 61"]}, 
+                    {target: '350 V2 Flax', fullRGB: ["228 222 190", "233 229 180", "195 186 144"]}, 
+                    {target: 'QNTM Lifestyle Model', fullRGB: ["165 163 166", "37 39 40", "233 230 219"]}, 
+                    {target: '700 MNVN Triple Black', fullRGB: ["35 37 38", "38 42 43", "60 63 69"]}, 
+                    {target: '350 V2 Marsh', fullRGB: ["191 188 136", "140 103 61", "156 156 156"]}, 
+                    {target: '350 V2 Yeshaya', fullRGB: ["227 226 220", "151 151 151", "215 214 192"]}, 
+                    {target: '700 V3 Azael', fullRGB: ["236 232 218", "18 20 22", "162 155 155"]}, 
+                    {target: '350 V2 Yecheil', fullRGB: ["35 35 37", "91 40 44", "223 230 203"]}, 
+                    {target: '700 Carbon Blue', fullRGB: ["143 162 171", "155 153 154", "255 154 84"]}, 
+                    {target: '350 V2 Yeezreel', fullRGB: ["199 238 182", "210 229 138", "74 91 72"]}, 
+                    {target: '380 Alien', fullRGB: ["236 240 229", "158 157 164", "224 237 214"]}, 
+                    {target: '500 Stone', fullRGB: ["211 213 195", "138 132 113", "186 187 166"]}, 
+                    {target: '500 Soft Vision', fullRGB: ["201 179 198", "195 173 182", "185 168 173"]}, 
+                    {target: '700 Teal Blue', fullRGB: ["90 102 107", "101 120 129", "79 80 88"]}, 
+                    {target: '700 V2 Hospital Blue', fullRGB: ["145 154 165", "160 186 195", "139 152 168"]}, 
+                    {target: '350 V2 Citrin', fullRGB: ["215 214 203", "222 213 180", "132 132 118"]}, 
+                    {target: '350 V2 Cloud White', fullRGB: ["219 226 229", "226 223 210", "150 167 177"]}, 
+                    {target: '700 Magnet', fullRGB: ["86 91 101", "242 151 111", "167 170 178"]}, 
+                    {target: '700 V2 Inertia', fullRGB: ["141 155 169", "253 158 113", "253 160 114"]}, 
+                    {target: '500 Bone White', fullRGB: ["245 242 234", "214 206 197", "210 207 199"]}, 
+                    {target: '350 V2 Lundmark', fullRGB: ["241 237 229", "196 183 167", "162 160 161"]}, 
+                    {target: '700 Utility Black', fullRGB: ["30 30 32", "59 61 65", "41 43 49"]}, 
+                    {target: '350 V2 Antlia', fullRGB: ["200 196 138", "172 170 123", "239 237 208"]}, 
+                    {target: '350 V2 Synth', fullRGB: ["209 199 202", "179 152 147", "234 208 187"]}, 
+                    {target: '700 V2 Tephra', fullRGB: ["164 162 164", "108 114 121", "61 70 78"]}, 
+                    {target: '350 V2 Black', fullRGB: ["39 42 50", "120 37 45", "30 31 35"]}, 
+                    {target: '700 V2 Vanta', fullRGB: ["21 20 25", "34 33 38", "52 50 62"]}, 
+                    {target: '350 V2 Glow', fullRGB: ["201 253 174", "232 236 171", "223 255 183"]}, 
+                    {target: '700 Analog', fullRGB: ["234 233 229", "132 143 162", "234 231 237"]}, 
+                    {target: '350 V2 Clay', fullRGB: ["250 180 154", "201 118 112", "255 201 131"]}, 
+                    {target: '700 V2 Geode', fullRGB: ["77 77 80", "57 57 59", "50 61 76"]}, 
+                    {target: '350 V2 Trfrm', fullRGB: ["208 205 207", "242 162 136", "218 188 177"]}, 
+                    {target: '350 V2 Hyperspace', fullRGB: ["235 238 230", "218 195 150", "224 237 212"]}, 
+                    {target: '700 Inertia', fullRGB: ["141 155 169", "253 158 113", "251 176 160"]}, 
+                    {target: '700 Salt', fullRGB: ["176 180 177", "178 175 165", "199 211 207"]}, 
+                    {target: '700 V2 Static', fullRGB: ["240 242 245", "158 163 177", "248 247 243"]}, 
+                    {target: '350 V2 Static', fullRGB: ["224 224 240", "91 93 108", "246 246 244"]}, 
+                    {target: '500 Salt', fullRGB: ["176 180 179", "190 189 185", "189 191 195"]}, 
+                    {target: '350 V2 Sesame', fullRGB: ["212 209 202", "211 202 175", "206 201 201"]}, 
+                    {target: '700 Mauve', fullRGB: ["76 71 78", "179 218 116", "41 46 53"]}, 
+                    {target: '500 Utility Black', fullRGB: ["51 51 53", "36 38 37", "62 61 66"]}, 
+                    {target: '350 V2 Butter', fullRGB: ["221 219 195", "232 239 195", "208 214 182"]}, 
+                    {target: '500 Super Moon Yellow', fullRGB: ["224 221 197", "188 185 140", "228 224 191"]}, 
+                    {target: '500 Blush', fullRGB: ["224 217 204", "184 170 154", "192 182 163"]}, 
+                    {target: '350 V2 Blue Tint', fullRGB: ["227 234 232", "224 68 64", "106 108 105"]}, 
+                    {target: '350 V2 Beluga 2.0', fullRGB: ["136 133 134", "244 82 80", "136 136 136"]}, 
+                    {target: '350 V2 Semi Frozen Yellow', fullRGB: ["202 219 127", "192 71 30", "82 89 103"]}, 
+                    {target: '700 Wave Runner Solid Grey', fullRGB: ["179 177 174", "255 123 67", "5 9 3"]}, 
+                    {target: '350 V2 Cream Triple White', fullRGB: ["245 242 240", "234 233 227", "238 239 227"]}, 
+                    {target: '350 V2 Zebra', fullRGB: ["228 227 234", "204 49 49", "55 58 57"]}, 
+                    {target: '350 V2 Black Red', fullRGB: ["31 31 33", "169 25 30", "46 44 40"]}, 
+                    {target: '350 V2 Core Black White', fullRGB: ["23 23 23", "198 190 170", "54 53 46"]}, 
+                    {target: '350 V2 Core Black Red', fullRGB: ["12 16 17", "218 47 47", "24 24 26"]}, 
+                    {target: '350 V2 Core Black Green', fullRGB: ["18 20 22", "112 105 94", "23 24 28"]}, 
+                    {target: '350 V2 Core Black Copper', fullRGB: ["25 27 31", "147 112 99", "31 33 37"]}, 
+                    {target: '350 V2 Beluga', fullRGB: ["122 118 116", "246 84 45", "58 54 49"]}, 
+                    {target: '350 Pirate Black 2016', fullRGB: ["17 20 19", "155 46 52", "22 27 28"]}, 
+                    {target: '350 Oxford Tan', fullRGB: ["208 199 182", "145 134 116", "192 171 147"]}, 
+                    {target: '350 Moonrock', fullRGB: ["170 168 162", "67 66 59", "163 160 154"]}, 
+                    {target: '350 Pirate Black 2015', fullRGB: ["34 29 32", "141 48 47", "49 44 48"]}, 
+                    {target: '350 Turtledove', fullRGB: ["166 166 164", "245 243 230", "70 68 66"]}]
+
+  // Euclidean Distance formula
+  function _distance(v1, v2) {
+    var total = 0;
+    for (var i = 0; i < v1.length; i++) {
+      total += Math.pow(v2[i] - v1[i], 2);      
+    }
+    return Math.sqrt(total);
+  }
+  function _findIdx(arr, initItem) {
+    let index = parseInt(arr.map((item, index) => item.target == initItem && index).reduce((a, b) => a + b));
+    return index
+  }
+  function _popNReminder(arr, initItem) {
+    let copyArr = [...arr];
+    arr.filter((item, index) => item.target == initItem && copyArr.splice(index, 1))
+    return copyArr
+  }
+
+  let saveData = (function () {
+    let a = document.createElement("a");
+    document.body.appendChild(a);
+    a.style = "display: none";
+    return function (data, fileName) {
+        var json = JSON.stringify(data),
+            blob = new Blob([json], {type: "octet/stream"}),
+            url = window.URL.createObjectURL(blob);
+        a.href = url;
+        a.download = fileName;
+        a.click();
+        window.URL.revokeObjectURL(url);
+    };
+  }());
+
+  let initItem = '350 V2 Core Black Red'
+  let initIdx = _findIdx(colorWayNname, initItem)
+  let deltaECollection = [{target: initItem, fullRGB: colorWayNname[initIdx].fullRGB, distance_primary: 0}]
+
+  function getPrimaryColor(arr, name) {
+    let reminderArr = _popNReminder(arr, name)
+    let targetIdx = _findIdx(arr, name)
+
+    let tempValue = [];
+    let tempName = [];
+    if (reminderArr.length != 0){
+      reminderArr.forEach(item => { 
+        let distance = parseFloat(_distance(item.fullRGB[0].split(' ').map(d => parseInt(d)), arr[targetIdx].fullRGB[0].split(' ').map(d => parseInt(d))).toFixed(2));
+        tempValue.push(distance)
+        tempName.push(item.target)
+      })
+      let minDst = Math.min(...tempValue)
+      let minIdx = tempValue.indexOf(minDst)
+      let minName = tempName[minIdx]
+      deltaECollection.push({target: minName, fullRGB: reminderArr[minIdx].fullRGB, distance_primary: minDst})
+      getPrimaryColor(reminderArr, minName);
+    } else {
+      let num = 0;
+      deltaECollection.forEach(item => {
+        let distance_highlight = parseFloat(_distance(item.fullRGB[0].split(' ').map(d => parseInt(d)), item.fullRGB[1].split(' ').map(d => parseInt(d))).toFixed(2));
+        item.distance_highlight = distance_highlight
+
+        if (item.distance_primary < 30) {
+          item.cluster = `cluster${num}`
+        } else if (item.distance_primary > 30) {
+          num += 1
+          item.cluster = `cluster${num}`
+        }
+      })
+      // saveData(deltaECollection, 'deltaE.json');
+      return;
+    }
+  }
+
+  getPrimaryColor(colorWayNname, initItem)
+
+  
